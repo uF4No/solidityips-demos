e3e4f5083d01dc864234e1be0c5e7f710cf77c3e
Antonio <tonioufa@gmail.com>
2021-11-10 15:42:40 +0000

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="my-8">About</h1>
+    <p>Created by Antonio Ufano</p>
+    <a href="http://soliditytips.com" target="_blank" rel="noopener noreferrer"
+      >SolidityTips.com</a
+    >
   </div>
 </template>
